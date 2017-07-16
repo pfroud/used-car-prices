9b7151aaab713afd6bb4be739f6c92be15acde2a
Peter Froud <pfroud@gmail.com>
2017-07-16 01:40:18 -0700

    Scraped first data

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import requests
+from bs4 import BeautifulSoup
+from base64 import b64decode
+
+
+class Car:
+    def __init__(self, dealer, year, model, trim, miles, price):
+        self.dealer = dealer
+        self.year = year
+        self.model = model
+        self.trim = trim
+        self.miles = miles
+        self.price = price
 
 
 def main():
-    pass
+    r = requests.get("https://www.toyotasunnyvale.com/used-inventory/index.htm?make=Toyota&model=Camry")
+    r.raise_for_status()
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    for div in soup.select("div.hproduct"):
+        mileage = div.find(lambda e: e.text == "Mileage:").find_next_sibling("dd").get_text()
+        car = Car("sunnyvale", int(div["data-year"]), div["data-model"], div["data-trim"], int(mileage),
+                  float(b64decode(div["data-internetprice"])))
+
+main()
